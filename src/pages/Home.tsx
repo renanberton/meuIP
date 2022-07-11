affe470077ede8b7240d483fbbe22cbb85f3cbbd
renanberton <renan_berton@yahoo.com.br>
2022-07-11 15:24:50 -0300

    First commit + Funfando

---
+++ src/pages/Home.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react';
+import './style.scss';
+
+type IpType = {
+    query: string,
+    country: string,
+    regionName: string,
+    city: string,
+    timezone: string,
+    isp: string,
+
+}
+
+export function Home() {
+    const [ip, setIp] = useState<IpType>();
+
+    useEffect(() => {
+        function LoadCEP() {
+            fetch('http://ip-api.com/json')
+                .then(response => response.json())
+                .then(data => setIp(data))
+
+        }
+        LoadCEP()
+        /* Aqui chamamos a função que faz os fetchs, etc e depois retorna o HTML abaixo \/ */
+
+    }, [ip])
+    return (
+        <div className='boxIp'>
+            <div>
+                <h1>Descubra seu IP</h1>
+                <ul>
+                    <li>
+                        Seu IP: {ip?.query}
+                    </li>
+                    <li>
+                        Pais de origem: {ip?.country}
+                    </li>
+                    <li>
+                        Cidade de Origem: {ip?.city}
+                    </li>
+                    <li>
+                        Região: {ip?.regionName}
+                    </li>
+                    <li>
+                        Operadora: {ip?.isp}
+                    </li>
+                    <li>
+                        Furo horário: {ip?.timezone}
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+
+}
+
