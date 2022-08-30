5ef42126dd3148446fbc70e8f5b70ae703b5b560
renanberton <renan_berton@yahoo.com.br>
2022-08-30 15:15:28 -0300

    Feature: Adicionado o iframe com o Google Maps, adicionado favicon de todos os dispositivo, add og:description e og:image

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,11 +6,13 @@ type IpType = {
     country: string,
     region: string,
     city: string,
+    latitude: string,
+    longitude: string,
 }
 
 export function Home() {
     const [ip, setIp] = useState<IpType>();
-
+    const zoom = 15;
     useEffect(() => {
         function LoadCEP() {
             fetch('https://ipwho.is/')
@@ -19,30 +21,31 @@ export function Home() {
 
         }
         LoadCEP()
-        /* Aqui chamamos a função que faz os fetchs, etc e depois retorna o HTML abaixo \/ */
-
     }, [ip])
+
+
     return (
-        <div className="container">
-
-            <div className='boxIp'>
-                <div>
-                    <h1>Meu IP</h1>
-                    <ul>
-                        <li>
-                            Seu IP: {ip?.ip}
-                        </li>
-                        <li>
-                            Pais de origem: {ip?.country}
-                        </li>
-                        <li>
-                            Cidade de Origem: {ip?.city}
-                        </li>
-                        <li>
-                            Região: {ip?.region}
-                        </li>
-
-                    </ul>
+        <div>
+            <div className="container">
+                <div className='boxIp'>
+                    <div>
+                        <h1>Meu IP</h1>
+                        <ul>
+                            <li>
+                                Seu IP: {ip?.ip}
+                            </li>
+                            <li>
+                                Pais de origem: {ip?.country}
+                            </li>
+                            <li>
+                                Cidade de Origem: {ip?.city}
+                            </li>
+                            <li>
+                                Região: {ip?.region}
+                            </li>
+                        </ul>
+                    </div>
+                    <iframe src={`https://maps.google.com/maps?q=@${ip?.latitude},${ip?.longitude}&z=${zoom}&output=embed`} />
                 </div>
             </div>
         </div>
